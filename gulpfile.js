d88252108334f38568b5eed0d363f32e51c960e6
HuynhThiThanhTuyen <58364854+HuynhThiThanhTuyen@users.noreply.github.com>
2020-06-05 22:14:53 +0700

    up

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
+
+var sass = require('gulp-sass');
+//style paths
+var sassFiles = 'sass/*.scss',
+    cssDest = 'css';
+
+gulp.task('styles', function() {
+    gulp.src(sassFiles)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(cssDest));
+});
+
+gulp.task('sass', function() {
+    gulp.watch(sassFiles, ['styles']);
+});
+
+// gulp.task('server', gulp.series('build', function() {
+//     browser.init({ server: './_site', port: port });
+// }));
+
+gulp.task('serve', ['build'], function() {
+    browserSync({
+        notify: false,
+        server: {
+            baseDir: '.'
+        }
+    });
+    gulp.watch(['*.html'], reload);
+    gulp.watch(['js/*.js'], reload);
+    gulp.watch(['css/*.css'], reload);
+    gulp.watch(sassFiles, ['sass'], reload);
+
+});
